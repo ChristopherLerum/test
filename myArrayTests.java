a250a00f6f12ef21e472c606ba9d98807416d67d
ChristopherLerum <146862377+ChristopherLerum@users.noreply.github.com>
2023-11-05 21:44:23 -0800

    Update myArrayTests.java

--- myArrayTests.java
+++ myArrayTests.java
@@ -4,15 +4,14 @@ import org.junit.*;
 public class myArrayTests {
         @Test
         public void testReverseInPlace() {
-    int[] input1 = {  1,2,3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3,2,1 }, input1);
+            int[] input1 = { 1,2,3 };
+            ArrayExamples.reverseInPlace(input1);
+            assertArrayEquals(new int[]{ 3,2,1 }, input1);
         }
-
-
-  @Test
-  public void testReversed() {
-    int[] input1 = { 1 };
-    assertArrayEquals(new int[]{ 1 }, ArrayExamples.reversed(input1));
-  }
+        @Test
+        public void testReverseInPlace() {
+            int[] input1 = { 1 };
+            ArrayExamples.reverseInPlace(input1);
+            assertArrayEquals(new int[]{ 1 }, input1);
+        }
 }
