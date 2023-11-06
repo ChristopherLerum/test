bd1d587f6e1870629a0fab25ecf45f272ff5ef5a
ChristopherLerum <146862377+ChristopherLerum@users.noreply.github.com>
2023-11-05 21:28:55 -0800

    Create myArrayTests.java

---
+++ myArrayTests.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ArrayTests {
+        @Test
+        public void testReverseInPlace() {
+    int[] input1 = {  1,2,3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3,2,1 }, input1);
+        }
+
+
+  @Test
+  public void testReversed() {
+    int[] input1 = { 1 };
+    assertArrayEquals(new int[]{ 1 }, ArrayExamples.reversed(input1));
+  }
+}
